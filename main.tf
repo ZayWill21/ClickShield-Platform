24d7137d134cc2722e2a6db609635d5b9083c4a2
ZayWill21 <isaiahgwilliam@gmail.com>
2026-05-18 20:08:02 -0700

    Removed the workspace key vlaue un the terraform pipeline and use a variable.

--- main.tf
+++ main.tf
@@ -2,11 +2,18 @@
      terraform {
        backend "remote" {
          hostname = "app.terraform.io"
+         workspaces {
+           name = var.tf_workspaces
+         }
        }
      }
-
 resource "null_resource" "example" {
        triggers = {
          value = "A example resource that does nothing!"
        }
+     }
+
+variable "tfcloud_workspace" {
+       type = string
+       description = "The name of the my tf cloud workspace "
      }
