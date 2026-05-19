ad5c9750fee5f57188728be1c90ed7455b3e14b9
ZayWill21 <isaiahgwilliam@gmail.com>
2026-05-18 19:56:40 -0700

    pass the organization and workspace names during the pipeline run using secrets.

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
 # The configuration for the `remote` backend.
      terraform {
        backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "var.tfcloud_org"
-
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "var.tfcloud_workspace"
-         }
+         hostname = "app.terraform.io"
        }
      }
-     
+
 resource "null_resource" "example" {
        triggers = {
          value = "A example resource that does nothing!"
