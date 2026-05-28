4e6a2e22cde54cc8312768a56683ecdb0cf675c3
ZayWill21 <isaiahgwilliam@gmail.com>
2026-05-27 21:09:44 -0700

    Configured AWS permissions created a workflow to test these cred. Switched from Remote backend to s3 for terrform.

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
 # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-         hostname = "app.terraform.io"
-         workspaces {
-           name = "ClickShield-Plateform"
-         }
-       }
-     }
+terraform {
+  backend "s3" {
+    bucket = secret("S3_BUCKETNAME")
+    key    = secret("S3_KEY_PATH")
+    region = secret("AWS_REGION")
+  }
+}
+
 resource "null_resource" "example" {
        triggers = {
          value = "A example resource that does nothing!"
        }
      }
 
-variable "tfcloud_workspace" {
-       type = string
-       description = "The name of the my tf cloud workspace "
-     }
