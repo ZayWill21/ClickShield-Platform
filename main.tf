9d00c77477f8cd84d5a5b44a439a392198d6f78a
ZayWill21 <isaiahgwilliam@gmail.com>
2026-05-27 21:59:42 -0700

    Change from secrets to var in tf main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # The configuration for the `remote` backend.
 terraform {
   backend "s3" {
-    bucket = secret("S3_BUCKETNAME")
-    key    = secret("S3_KEY_PATH")
-    region = secret("AWS_REGION")
+    bucket = var.S3_BUCKETNAME
+    key    = var.S3_KEY_PATH
+    region = var.AWS_REGION
   }
 }
 
