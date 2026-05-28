218b13c708fbfad74cbe5e382775ac53e2391c61
ZayWill21 <isaiahgwilliam@gmail.com>
2026-05-28 11:45:37 -0700

    Applied the secrets as variables

--- Infra/terraform/networking.tf
+++ Infra/terraform/networking.tf
@@ -47,7 +47,7 @@ resource "aws_route_table" "public_rt" {
   depends_on = [ aws_internet_gateway.gw ]
   vpc_id = aws_vpc.vpc_main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.ZEROS
     gateway_id = aws_internet_gateway.gw.id
   }
     tags = {
