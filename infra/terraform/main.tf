1c8d9bd7230ac7db4d0f817ec35717155d4392d4
ZayWill21 <isaiahgwilliam@gmail.com>
2026-05-30 00:24:20 -0700

    Fix working directory path

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_ebs_volume" "aws" {
 }
 
 module "networking" {
-  source = "./infra/terraform/aws/networking"
+  source = "/aws/networking"
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   availability_zones   = var.availability_zones
