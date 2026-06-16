6d6fe6024de8d028706b40bf121d3f506103a191
ZayWill21 <isaiahgwilliam@gmail.com>
2026-06-15 20:20:44 -0700

    Deploy KMS

--- infra/terraform/aws/networking/main.tf
+++ infra/terraform/aws/networking/main.tf
@@ -26,17 +26,41 @@ resource "aws_kms_key" "flow_log" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "Allow account use of the key"
-        Effect    = "Allow"
-        Principal = { AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" }
-        Action    = "kms:*"
-        Resource  = "*"
+        "Sid": "Allow account use of the key"
+        "Effect": "Allow"
+        "Principal": { "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" }
+        "Action": "kms:*"
+        "Resource": "*"
       },
       {
-        Sid       = "Allow CloudWatch Logs use of the key"
-        Effect    = "Allow"
-        Principal = { Service = "logs.amazonaws.com" }
-        Action = [
+        "Sid": "Allow access for Key Administrators"
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GitHub_Actions_Role"]
+        },
+        "Action": [
+              "kms:Create*",
+              "kms:Describe*",
+              "kms:Enable*",
+              "kms:List*",
+              "kms:Put*",
+              "kms:Update*",
+              "kms:Revoke*",
+              "kms:Disable*",
+              "kms:Get*",
+              "kms:Delete*",
+              "kms:TagResource",
+              "kms:UntagResource",
+              "kms:ScheduleKeyDeletion",
+              "kms:CancelKeyDeletion"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "Allow CloudWatch Logs use of the key"
+        "Effect": "Allow"
+        "Principal": { "Service" = "logs.amazonaws.com" }
+        "Action": [
           "kms:Decrypt",
           "kms:Encrypt",
           "kms:ReEncrypt*",
