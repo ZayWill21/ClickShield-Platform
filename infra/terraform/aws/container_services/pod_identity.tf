9c776cb4450443a0ddcf49fe168b3c3588625947
ZayWill21 <isaiahgwilliam@gmail.com>
2026-06-20 16:21:42 -0700

    Fix network flow jsonencode

--- infra/terraform/aws/container_services/pod_identity.tf
+++ infra/terraform/aws/container_services/pod_identity.tf
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "network-flow-monitor-agent-role" {
   name = "network-flow-monitor-agent-role"
   description = "IAM role for AWS Network Flow Monitoring Agent"
-  assume_role_policy = jsonencode(data.aws_iam_policy_document.assume_role.json)
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "network-flow-monitor-agent-policy-attachment" {
